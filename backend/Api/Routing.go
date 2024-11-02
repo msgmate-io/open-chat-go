f41536681df2eadf2ab9fe20ac05c3f8a65bc4a9
Tim Schupp <tim@timschupp.de>
2024-11-02 15:22:13 +0100

    implemented simple Open-Api-Schema integration

--- backend/Api/Routing.go
+++ backend/Api/Routing.go
@@ -3,12 +3,20 @@ package Api
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/swaggo/http-swagger"
 	"net/http"
 )
 
 func SetupRouting() *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	r.Get("/api/schema/", SchemaHandler)
+
+	r.Get("/api/schema/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:3000/api/schema/"), //The url pointing to API definition
+	))
+
 	r.Get("/hello", UserSelfHandler)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,8 @@ Notes:
 
 - maybe introduce for rest documentation https://github.com/swaggest/rest/
 - open-chat's python api spec: https://beta.msgmate.io/api/schema/swagger-ui/
+- openapi choices: https://www.reddit.com/r/golang/comments/1avsog1/go_openapi_codegen/
+- `$HOME/go/bin/swag init`
 
 */
 
@@ -18,12 +20,20 @@ import (
 const serverPort int = 3000
 const dbBackend Models.DbBackend = Models.SqLite
 
+// @title Open-Chat 2.0 API
+// @version 3.1
+// @description Hello there :)
+// @termsOfService http://swagger.io/terms/
+
+// @host beta.msgmate.io
+// @BasePath /v2
 func main() {
 	// 1 - Setup the database
 	Models.SetupDatabase(dbBackend)
 
 	// 2 - Setup the router
 	r := Api.SetupRouting()
-	fmt.Printf("Server running on port %d\n", serverPort)
+
+	fmt.Printf("Server running on http://localhost:%d\n", serverPort)
 	http.ListenAndServe(fmt.Sprintf(":%d", serverPort), r)
 }
