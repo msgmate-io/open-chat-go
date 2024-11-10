d43f355611d0aa263d61f0a9e153e88359eef617
saces <saces@c-base.org>
2024-11-10 12:16:59 +0100

    no need for local variables

--- backend/cmd/server.go
+++ backend/cmd/server.go
@@ -4,15 +4,11 @@ import (
 	"backend/server"
 	"context"
 	"fmt"
+
 	"github.com/urfave/cli/v3"
 )
 
 func ServerCli() *cli.Command {
-	var port int64
-	var host string
-	var debug bool
-	var ssl bool
-
 	cmd := &cli.Command{
 		Name:  "boom",
 		Usage: "make an explosive entrance",
@@ -25,41 +21,37 @@ func ServerCli() *cli.Command {
 				Usage:   "database driver to use",
 			},
 			&cli.BoolFlag{
-				Sources:     cli.EnvVars("DEBUG"),
-				Name:        "debug",
-				Destination: &debug,
-				Aliases:     []string{"d"},
-				Value:       false,
-				Usage:       "enable debug mode",
+				Sources: cli.EnvVars("DEBUG"),
+				Name:    "debug",
+				Aliases: []string{"d"},
+				Value:   false,
+				Usage:   "enable debug mode",
 			},
 			&cli.StringFlag{
-				Sources:     cli.EnvVars("HOST"),
-				Name:        "host",
-				Destination: &host,
-				Aliases:     []string{"b"},
-				Value:       "127.0.0.1",
-				Usage:       "server bind address",
+				Sources: cli.EnvVars("HOST"),
+				Name:    "host",
+				Aliases: []string{"b"},
+				Value:   "127.0.0.1",
+				Usage:   "server bind address",
 			},
 			&cli.BoolFlag{
-				Sources:     cli.EnvVars("SSL"),
-				Name:        "ssl",
-				Destination: &ssl,
-				Aliases:     []string{"s"},
-				Value:       false,
-				Usage:       "enable ssl",
+				Sources: cli.EnvVars("SSL"),
+				Name:    "ssl",
+				Aliases: []string{"s"},
+				Value:   false,
+				Usage:   "enable ssl",
 			},
 			&cli.IntFlag{
-				Sources:     cli.EnvVars("PORT"),
-				Name:        "port",
-				Destination: &port,
-				Aliases:     []string{"p"},
-				Value:       1984,
-				Usage:       "server port",
+				Sources: cli.EnvVars("PORT"),
+				Name:    "port",
+				Aliases: []string{"p"},
+				Value:   1984,
+				Usage:   "server port",
 			},
 		},
-		Action: func(context.Context, *cli.Command) error {
+		Action: func(_ context.Context, c *cli.Command) error {
 
-			s, fullHost := server.BackendServer(host, port, debug, ssl)
+			s, fullHost := server.BackendServer(c.String("host"), c.Int("port"), c.Bool("debug"), c.Bool("ssl"))
 			fmt.Printf("Starting server on %s\n", fullHost)
 
 			s.ListenAndServe()
