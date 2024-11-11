a53e4a376ff8dff1c2dee7f36a9ec36d11a73c9f
Tim Schupp <tim@timschupp.de>
2024-11-11 15:09:37 +0100

    simple bcrypt password hash check for user login hander

--- backend/api/user.go
+++ backend/api/user.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"net/mail"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserHandler struct{}
@@ -16,8 +18,10 @@ type UserLogin struct {
 }
 
 // curl -X POST -H "Content-Type: application/json" -d '{"email":"tim+test@timschupp.de","password":"password"}' http://localhost:1984/api/v1/user/login
+// https://stackoverflow.com/questions/23259586/bcrypt-password-hashing-in-golang-compatible-with-node-js
 func (h *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
 	var data UserLogin
+	var defaultErrorMessage string = "Invalid email or password"
 
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
@@ -26,23 +30,30 @@ func (h *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
 
 	_, err := mail.ParseAddress(data.Email)
 	if err != nil {
-		http.Error(w, "Invalid email", http.StatusBadRequest)
+		http.Error(w, defaultErrorMessage, http.StatusBadRequest)
 		return
 	}
 
 	if data.Password == "" {
-		http.Error(w, "Invalid password", http.StatusBadRequest)
+		http.Error(w, defaultErrorMessage, http.StatusBadRequest)
 		return
 	}
 
 	var user database.User
-	q := database.DB.First(&user, "email = ?", "tim+test@timschupp.de")
+	q := database.DB.First(&user, "email = ?", data.Email)
 
 	if q.Error != nil {
-		http.Error(w, "User not found", http.StatusNotFound)
+		http.Error(w, defaultErrorMessage, http.StatusNotFound)
 		return
 	}
 
-	log.Println(user)
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(data.Password))
+	if err != nil {
+		http.Error(w, defaultErrorMessage, http.StatusUnauthorized)
+		return
+	}
 
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Login successful"))
+	// TODO: generate session token and return it as `Set-Cookie` header
 }
