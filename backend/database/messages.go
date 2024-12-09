bdca38895f00575f5e57eb654011861b30abc83e
Tim Schupp <tim@timschupp.de>
2024-12-09 18:26:53 +0100

    adjusted message model

--- backend/database/messages.go
+++ backend/database/messages.go
@@ -3,12 +3,15 @@ package database
 // Some inspiration from: https://github.com/omept/go-chat/
 type Message struct {
 	Model
-	SenderId   uint   `json:"SenderId" gorm:"index"`
-	Sender     User   `json:"User" gorm:"foreignKey:SenderId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:NO ACTION;"`
-	ReceiverId uint   `json:"ReceiverId" gorm:"index"`
-	Receiver   User   `json:"Receiver" gorm:"foreignKey:ReceiverId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:NO ACTION;"`
-	DataType   string `json:"DataType" gorm:"default:'text'"`
-	Content    []byte `json:"Content"`
+	SenderId   uint    `json:"-" gorm:"index"`
+	Sender     User    `json:"-" gorm:"foreignKey:SenderId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:NO ACTION;"`
+	ReceiverId uint    `json:"-" gorm:"index"`
+	Receiver   User    `json:"-" gorm:"foreignKey:ReceiverId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:NO ACTION;"`
+	DataType   string  `json:"data_type" gorm:"default:'text'"`
+	ChatId     uint    `json:"-" gorm:"index"`
+	Chat       Chat    `json:"-" gorm:"foreignKey:ChatId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:NO ACTION;"`
+	Content    *[]byte `json:"-"`
+	Text       *string `json:"text"`
 }
 
 type Chat struct {
@@ -17,16 +20,14 @@ type Chat struct {
 	User2Id         uint     `json:"-" gorm:"index"`
 	User1           User     `json:"user1" gorm:"foreignKey:User1Id;references:ID;constraint:OnUpdate:CASCADE,OnDelete:NO ACTION;"`
 	User2           User     `json:"user2" gorm:"foreignKey:User2Id;references:ID;constraint:OnUpdate:CASCADE,OnDelete:NO ACTION;"`
-	LatestMessageId *uint    `json:"-" gorm:"index"` // Make this a pointer
+	LatestMessageId *uint    `json:"-" gorm:"index"`
 	LatestMessage   *Message `json:"latest_message" gorm:"foreignKey:LatestMessageId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:NO ACTION;"`
-	// TODO Chat Settings?
 }
 
 type ChatSettings struct {
 	Model
 	ChatId uint `json:"ChatId" gorm:"index"`
 	Chat   Chat `json:"Chat" gorm:"foreignKey:ChatId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:NO ACTION;"`
-	// TODO
 }
 
 type Contact struct {
