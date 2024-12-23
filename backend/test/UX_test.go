c381a362e37c355ec7f11d5de18212ab4b567397
Tim Schupp <tim@timschupp.de>
2024-12-23 19:08:11 +0100

    UX test can now also test the websocket implementations

--- backend/test/UX_test.go
+++ backend/test/UX_test.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"backend/api/chats"
+	"backend/api/contacts"
 	"backend/api/user"
 	"backend/cmd"
 	"backend/database"
@@ -9,10 +11,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 	"testing"
 	"time"
 )
@@ -152,6 +157,70 @@ func getUserInfo(host string, sessionId string) (error, *database.User) {
 	return nil, &user
 }
 
+func addContact(host string, sessionId string, data contacts.AddContact) error {
+	body := new(bytes.Buffer)
+	err := json.NewEncoder(body).Encode(data)
+	if err != nil {
+		log.Printf("Erroror encoding data: %v", err)
+		return err
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/v1/contacts/add", host), body)
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Origin", host)
+	req.Header.Set("Cookie", fmt.Sprintf("session_id=%s", sessionId))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error sending request: %v", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Error response: %v", resp.Status)
+	}
+
+	return nil
+}
+
+func listContacts(host string, sessionId string) (error, *contacts.PaginatedContacts) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/v1/contacts/list", host), nil)
+	if err != nil {
+		log.Printf("Error creating request: %v", err)
+		return err, nil
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Origin", host)
+	req.Header.Set("Cookie", fmt.Sprintf("session_id=%s", sessionId))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error sending request: %v", err)
+		return err, nil
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Error response: %v", resp.Status)
+		return err, nil
+	}
+
+	var contactsPaginated contacts.PaginatedContacts
+	err = json.NewDecoder(resp.Body).Decode(&contactsPaginated)
+
+	if err != nil {
+		log.Printf("Error decoding response: %v", err)
+		return err, nil
+	}
+
+	return nil, &contactsPaginated
+}
+
 func startTestServer(args []string) (error, string, context.CancelFunc) {
 	cmd := cmd.ServerCli()
 	os.Args = args
@@ -201,7 +270,74 @@ func startTestServer(args []string) (error, string, context.CancelFunc) {
 	return nil, host, cancel
 }
 
-// go test -v ./... -run "^Test_UXFlow$"
+func createChat(host string, sessionId string, data chats.CreateChat) (error, *database.Chat) {
+	body := new(bytes.Buffer)
+	err := json.NewEncoder(body).Encode(data)
+	if err != nil {
+		log.Printf("Erroror encoding data: %v", err)
+		return err, nil
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/v1/chats/create", host), body)
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Origin", host)
+	req.Header.Set("Cookie", fmt.Sprintf("session_id=%s", sessionId))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error sending request: %v", err)
+		return err, nil
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Error response: %v", resp.Status), nil
+	}
+
+	var chat database.Chat
+	err = json.NewDecoder(resp.Body).Decode(&chat)
+
+	if err != nil {
+		log.Printf("Error decoding response: %v", err)
+		return err, nil
+	}
+
+	return nil, &chat
+}
+
+func sendChatMessage(host string, sessionId string, chatUUID string, data chats.SendMessage) error {
+	body := new(bytes.Buffer)
+	err := json.NewEncoder(body).Encode(data)
+	if err != nil {
+		log.Printf("Erroror encoding data: %v", err)
+		return err
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/v1/chats/%s/messages/send", host, chatUUID), body)
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Origin", host)
+	req.Header.Set("Cookie", fmt.Sprintf("session_id=%s", sessionId))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error sending request: %v", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Error response: %v", resp.Status)
+	}
+
+	return nil
+
+}
+
+// 'go test -v ./... -run "^Test_UXFlow$"'
 func Test_UXFlow(t *testing.T) {
 	// what used to be _scripts/simple_api_test.sh
 	err, host, cancel := startTestServer([]string{"backend", "-b", "127.0.0.1", "-p", "1984", "-pp2p", "1985"})
@@ -271,8 +407,131 @@ func Test_UXFlow(t *testing.T) {
 	pretty, _ := json.MarshalIndent(userAInfo, "", "  ")
 	fmt.Println("User A info:", string(pretty))
 
+	err, userBInfo := getUserInfo(host, sessionIdB)
+	if err != nil {
+		t.Errorf("Error fetching user info: %v", err)
+	}
+
+	if userBInfo == nil {
+		t.Errorf("User B info is nil")
+	}
+
 	if userAInfo.Name != userA.Name {
 		t.Errorf("User A name mismatch: %v", userAInfo.Name)
+	}
+
+	err = addContact(host, sessionIdA, contacts.AddContact{
+		ContactToken: userBInfo.ContactToken,
+	})
+
+	if err != nil {
+		t.Errorf("Error adding contact: %v", err)
+	}
+
+	err = addContact(host, sessionIdB, contacts.AddContact{
+		ContactToken: userAInfo.ContactToken,
+	})
+
+	if err != nil {
+		t.Errorf("Error adding contact: %v", err)
+	}
+
+	err, contactsA := listContacts(host, sessionIdA)
+
+	if err != nil || contactsA == nil {
+		t.Errorf("Error listing contacts: %v", err)
+	}
+
+	pretty, _ = json.MarshalIndent(contactsA, "", "  ")
+	fmt.Println("Contacts A:", string(pretty))
+
+	err, contactsB := listContacts(host, sessionIdB)
+
+	if err != nil || contactsB == nil {
+		t.Errorf("Error listing contacts: %v", err)
+	}
+
+	pretty, _ = json.MarshalIndent(contactsB, "", "  ")
+	fmt.Println("Contacts B:", string(pretty))
+
+	// now we cen try connecting to the websocket
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	hostNoProtocol := strings.Replace(strings.Replace(host, "http://", "", 1), "https://", "", 1)
+	fmt.Println("Connecting to websocket", hostNoProtocol)
+
+	// create a new chat
+
+	err, chat := createChat(host, sessionIdA, chats.CreateChat{
+		ContactToken: userBInfo.ContactToken,
+	})
+	pretty, _ = json.MarshalIndent(chat, "", "  ")
+	fmt.Println("Chat:", string(pretty))
+
+	// ================== Websocket communication tests ==================
+
+	c, _, err := websocket.Dial(ctx, fmt.Sprintf("ws://%s/ws/connect", hostNoProtocol), &websocket.DialOptions{
+		HTTPHeader: http.Header{
+			"Cookie": []string{fmt.Sprintf("session_id=%s", sessionIdA)},
+		},
+	})
+	if err != nil {
+		t.Errorf("Error connecting to websocket")
+	}
+	defer c.CloseNow()
+
+	ctx2, cancel2 := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel2()
+
+	// Create a results channel
+	resultCh := make(chan interface{})
+	errorCh := make(chan error)
+
+	// Run the websocket reading in a separate goroutine
+	go func() {
+		var rawMessage json.RawMessage
+		err := wsjson.Read(ctx2, c, &rawMessage)
+		if err != nil {
+			errorCh <- err
+			return
+		}
+
+		// First attempt to unmarshal into a string
+		var messageString string
+		if err := json.Unmarshal(rawMessage, &messageString); err == nil {
+			resultCh <- messageString
+			return
+		}
+
+		// Fallback to more complex types
+		var messageMap map[string]interface{}
+		if err := json.Unmarshal(rawMessage, &messageMap); err == nil {
+			resultCh <- messageMap
+			return
+		}
+
+		// If neither unmarshal succeeded, report an error
+		errorCh <- fmt.Errorf("Unsupported message type")
+	}()
+
+	// Now send a message from user B to user A
+	time.Sleep(1 * time.Second)
+
+	err = sendChatMessage(host, sessionIdB, chat.UUID, chats.SendMessage{
+		Text: "Hello from user B",
+	})
+
+	// Wait for a message or timeout
+	select {
+	case msg := <-resultCh:
+		pretty, _ = json.MarshalIndent(msg, "", "  ")
+		fmt.Println("Message from websocket:", string(pretty))
+	case err := <-errorCh:
+		t.Errorf("Error reading message from websocket: %v", err)
+	case <-ctx2.Done():
+		t.Errorf("Timeout waiting for message from websocket")
 	}
 
 	cancel() // Stop the server
