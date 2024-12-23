47b5131f5851cef741d7743da584c1287b12601a
Tim Schupp <tim@timschupp.de>
2024-12-23 17:12:45 +0100

    a better UX test

--- backend/test/UX_test.go
+++ backend/test/UX_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"backend/api/user"
 	"backend/cmd"
+	"backend/database"
 	"backend/server"
 	"bytes"
 	"context"
@@ -116,6 +117,41 @@ func registerUser(host string, data user.UserRegister) error {
 	return nil
 }
 
+func getUserInfo(host string, sessionId string) (error, *database.User) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/v1/user/self", host), nil)
+	if err != nil {
+		log.Printf("Error creating request: %v", err)
+		return err, nil
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Origin", host)
+	req.Header.Set("Cookie", fmt.Sprintf("session_id=%s", sessionId))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error sending request: %v", err)
+		return err, nil
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Error response: %v", resp.Status)
+		return err, nil
+	}
+
+	var user database.User
+
+	err = json.NewDecoder(resp.Body).Decode(&user)
+	if err != nil {
+		log.Printf("Error decoding response: %v", err)
+		return err, nil
+	}
+
+	return nil, &user
+}
+
 func startTestServer(args []string) (error, string, context.CancelFunc) {
 	cmd := cmd.ServerCli()
 	os.Args = args
@@ -165,25 +201,43 @@ func startTestServer(args []string) (error, string, context.CancelFunc) {
 	return nil, host, cancel
 }
 
+// go test -v ./... -run "^Test_UXFlow$"
 func Test_UXFlow(t *testing.T) {
 	// what used to be _scripts/simple_api_test.sh
 	err, host, cancel := startTestServer([]string{"backend", "-b", "127.0.0.1", "-p", "1984", "-pp2p", "1985"})
 
 	fmt.Println("Registering user 1")
 
-	err = registerUser(host, user.UserRegister{
-		Name:     "User 1",
-		Email:    "herrduenschnlate+test1@gmail.com",
+	// Register User A
+	userA := user.UserRegister{
+		Name:     "User A",
+		Email:    "herrduenschnlate+testA@gmail.com",
 		Password: "password",
-	})
+	}
+
+	err = registerUser(host, userA)
 
 	if err != nil {
 		t.Errorf("Error registering user: %v", err)
 	}
 
-	err, sessionIdA := loginUser(host, user.UserLogin{
-		Email:    "herrduenschnlate+test1@gmail.com",
+	// Register User B
+	userB := user.UserRegister{
+		Name:     "User B",
+		Email:    "herrduenschnlate+testB@gmail.com",
 		Password: "password",
+	}
+
+	err = registerUser(host, userB)
+
+	if err != nil {
+		t.Errorf("Error registering user: %v", err)
+	}
+
+	// Login User A
+	err, sessionIdA := loginUser(host, user.UserLogin{
+		Email:    userA.Email,
+		Password: userA.Password,
 	})
 
 	if err != nil {
@@ -192,5 +246,34 @@ func Test_UXFlow(t *testing.T) {
 
 	fmt.Println("Session A:", sessionIdA)
 
-	cancel()
+	// Login User B
+	err, sessionIdB := loginUser(host, user.UserLogin{
+		Email:    userB.Email,
+		Password: userB.Password,
+	})
+
+	if err != nil {
+		t.Errorf("Error logging in user: %v", err)
+	}
+
+	fmt.Println("Session A:", sessionIdB)
+
+	// Try fetching self info
+	err, userAInfo := getUserInfo(host, sessionIdA)
+	if err != nil {
+		t.Errorf("Error fetching user info: %v", err)
+	}
+
+	if userAInfo == nil {
+		t.Errorf("User A info is nil")
+	}
+
+	pretty, _ := json.MarshalIndent(userAInfo, "", "  ")
+	fmt.Println("User A info:", string(pretty))
+
+	if userAInfo.Name != userA.Name {
+		t.Errorf("User A name mismatch: %v", userAInfo.Name)
+	}
+
+	cancel() // Stop the server
 }
