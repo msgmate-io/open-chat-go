848a3d062f43ea86682fb4c58b61b1e5121a7a45
Tim Schupp <tim@timschupp.de>
2025-02-05 01:55:29 +0100

    kill nextjs setup

--- frontend/nextjs/app/chat/[chat_uuid]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Chat from "@/next-components/Chat";
-
-export default function ChatPage() {
-  return <Chat />
-}
-
-export function generateStaticParams() {
-  return [{ chat_uuid: "$chat_uuid" }, { chat_uuid: "index" }];
-}
--- frontend/nextjs/dev.dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:22-alpine AS deps
-
-WORKDIR /frontend
-ENV NEXT_PUBLIC_SERVER_URL=http://backend:1984
-
-CMD sh -c "npm install | cat && npm run dev | cat"
