f8f4302167f43ad7bd1e7bc257d3d842f015c34f
Tim Schupp <tim@timschupp.de>
2025-02-10 11:02:59 +0100

    delete old frontend

--- old_remix_frontend/app/utils/auth.ts
+++
@@ -1,8 +0,0 @@
-export async function checkAuth() {
-  const response = await fetch("/auth-check", {
-    credentials: "include",
-  });
-  
-  const isAuthorized = response.headers.get("X-Authorized") === "true";
-  return { isAuthorized };
-} 
