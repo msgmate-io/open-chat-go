74e135160360ed4f77870af8557bba2e9f6c5432
Tim Schupp <tim@timschupp.de>
2025-02-10 10:25:31 +0100

    several frontend refactors and improvements

--- frontend/pages/chat/new/@contact_token/+Page.tsx
+++ frontend/pages/chat/new/@contact_token/+Page.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import { ChatBase } from "@/components/chat/ChatBase";
 import { navigate } from 'vike/client/router'
 import { StartChat } from "@/components/chat/StartChat";
+import useSWR, { mutate } from "swr"
 import { usePageContext } from "vike-react/usePageContext";
+import { fetcher } from "@/lib/utils";
 
 export default function Page() {
-const pageContext = usePageContext()
+  const pageContext = usePageContext()
 
   return <ChatBase chatUUID={null} navigateTo={(to: string) => {navigate(to)}}>
-        <StartChat contactToken={pageContext.routeParams.contact_token} leftPannelCollapsed={false} onToggleCollapse={() => {}} navigateTo={(to: string) => {navigate(to)}} />
+        <StartChat contactToken={pageContext.routeParams.contact_token} navigateTo={(to: string) => {navigate(to)}} />
     </ChatBase>
 }
