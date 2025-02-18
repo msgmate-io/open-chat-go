359a04686dc28a0503245d6ef95a3a0beeb5a7f8
Tim Schupp <tim@timschupp.de>
2025-02-18 02:30:43 +0100

    made table components reusable and used to start open-chat node display

--- frontend/pages/admin/@table_name/+Page.tsx
+++ frontend/pages/admin/@table_name/+Page.tsx
@@ -2,64 +2,17 @@ import React from "react";
 import { ColumnDef } from "@tanstack/react-table"
 import useSWR from "swr";
 import { usePageContext } from "vike-react/usePageContext";
-import { DataTable } from "./DataTable";
+import { DataTable } from "@/components/DataTable";
 import { Button } from "@/components/Button";
 import { Settings, ChevronLeft } from "lucide-react";
 import { navigate } from "vike/client/router";
+import { PaginationUI } from "@/components/PaginationUI";
 
 const fetcher = (...args: [RequestInfo, RequestInit?]) => fetch(...args).then(res => res.json())
 
 const routeBase = "/admin"
 export async function onBeforePrerenderStart() {
   return [routeBase, `${routeBase}/{table_name}`]
-}
-
-const PaginationUI = ({ page, totalPages, setPage, children }: { page: number, totalPages: number, setPage: (page: number) => void, children: React.ReactNode }) => {
-    return <div className="flex items-center justify-between sticky top-0 p-2 bg-base-100 z-10 shadow-sm">
-      <div className="text-sm text-gray-500">
-        {children}
-      </div>
-      <div className="space-x-2">
-        {page > 1 && <Button 
-          onClick={() => setPage(1)}
-          disabled={page === 1}
-          variant="outline"
-          size="sm"
-        >
-          0
-        </Button>}
-        {page > 0 && <Button 
-          onClick={() => setPage(p => Math.max(1, p - 1))}
-          disabled={page === 1}
-          variant="outline"
-          size="sm"
-        >
-          {page - 1}
-        </Button>}
-        <Button
-          variant="default"
-          size="sm"
-        >
-          {page}
-        </Button>
-        <Button
-          onClick={() => setPage(p => Math.min(totalPages, p + 1))}
-          disabled={page === totalPages}
-          variant="outline"
-          size="sm"
-        >
-          {page + 1}
-        </Button>
-        <Button
-          onClick={() => setPage(totalPages)}
-          disabled={page === totalPages}
-          variant="outline"
-          size="sm"
-        >
-          Last ({totalPages})
-        </Button>
-      </div>
-    </div>
 }
 
 export default function Page() {
