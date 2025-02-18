5ea0c54e9b740cbe8d2fc51655e76b169cd43b5d
Tim Schupp <tim@timschupp.de>
2025-02-18 02:30:43 +0100

    made table components reusable and used to start open-chat node display

--- frontend/pages/nodes/self/+Page.tsx
+++ frontend/pages/nodes/self/+Page.tsx
@@ -6,11 +6,21 @@ import { fetcher } from "@/lib/utils";
 export default function Page() {
     const pageContext = usePageContext()
     const { data: nodes } = useSWR(`/api/v1/federation/nodes/list`, fetcher)
+    const { data: identity } = useSWR(`/api/v1/federation/identity`, fetcher)
     
     return (
         <NodesPlattform descriptor="Selected Node">
             <div>
-            SELF PAge
+            <h1 className="text-3xl font-bold text-foreground">Your Node:</h1>
+            <h2 className="text-2xl font-bold text-foreground">Id: {identity?.id}</h2>
+            <h2 className="text-2xl font-bold text-foreground">Addresses:</h2>
+            <ul>
+                <li>
+                    {identity?.connect_multiadress?.map((address: any) => (
+                        <div key={address}>{address}</div>
+                    ))}
+                </li>
+            </ul>
             </div>
         </NodesPlattform>
     )
