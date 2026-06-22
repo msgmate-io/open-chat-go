ea4a54adb367b1fd899f7080c7d7860f527bb952
Tim Schupp <tim@timschupp.de>
2026-06-22 22:44:34 +0200

    ensure default bot connection for all users always

--- backend/server/server.go
+++ backend/server/server.go
@@ -34,69 +34,92 @@ func BackendServer(
 
 func SetupBaseConnections(
 	DB *gorm.DB,
-	adminUserId uint, baseBotId uint,
+	_ uint, baseBotId uint,
 ) error {
-	var adminUser database.User
-	if err := DB.First(&adminUser, "id = ?", adminUserId).Error; err != nil {
-		return err
-	}
-
 	var botUser database.User
 	if err := DB.First(&botUser, "id = ?", baseBotId).Error; err != nil {
 		return err
 	}
 
+	var users []database.User
+	if err := DB.Where("id <> ? AND is_automated = ?", botUser.ID, false).Find(&users).Error; err != nil {
+		return err
+	}
+
+	for _, user := range users {
+		if err := ensureUserConnectedToDefaultBot(DB, user, botUser); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func ensureUserConnectedToDefaultBot(DB *gorm.DB, user database.User, botUser database.User) error {
+	var contact database.Contact
+	err := DB.Where("owning_user_id = ? AND contact_user_id = ?", user.ID, botUser.ID).First(&contact).Error
+	if err != nil {
+		if err != gorm.ErrRecordNotFound {
+			return err
+		}
+
+		contact = database.Contact{
+			OwningUserId:  user.ID,
+			ContactUserId: botUser.ID,
+		}
+		if err := DB.Create(&contact).Error; err != nil {
+			return err
+		}
+	}
+
 	// first check if already a chat between these two exists
 	var chat database.Chat
-	DB.Where("user1_id = ? AND user2_id = ?", adminUser.ID, botUser.ID).First(&chat)
-	if chat.ID != 0 {
+	err = DB.Where(
+		"(user1_id = ? AND user2_id = ?) OR (user1_id = ? AND user2_id = ?)",
+		user.ID,
+		botUser.ID,
+		botUser.ID,
+		user.ID,
+	).First(&chat).Error
+	if err == nil {
 		return nil
 	}
-
-	// add to each others contacts
-	contact := database.Contact{
-		OwningUserId:  adminUser.ID,
-		ContactUserId: botUser.ID,
+	if err != gorm.ErrRecordNotFound {
+		return err
 	}
 
-	r := DB.Create(&contact)
-
-	if r.Error != nil {
-		return r.Error
+	if user.ID < botUser.ID {
+		chat = database.Chat{
+			User1Id: user.ID,
+			User2Id: botUser.ID,
+		}
+	} else {
+		chat = database.Chat{
+			User1Id: botUser.ID,
+			User2Id: user.ID,
+		}
 	}
-
-	chat = database.Chat{
-		User1Id: contact.OwningUserId,
-		User2Id: contact.ContactUserId,
-	}
-
-	r = DB.Create(&chat)
-
-	if r.Error != nil {
-		return r.Error
+	if err := DB.Create(&chat).Error; err != nil {
+		return err
 	}
 
 	// Now create a hello word message from the bot to the user
 	text := "Hello World"
 	message := database.Message{
 		SenderId:   botUser.ID,
-		ReceiverId: adminUser.ID,
+		ReceiverId: user.ID,
 		ChatId:     chat.ID,
 		Text:       &text,
 	}
 
-	r = DB.Create(&message)
-
-	if r.Error != nil {
-		return r.Error
+	if err := DB.Create(&message).Error; err != nil {
+		return err
 	}
 
 	// Now we update the chat again with the latest chat id
 	chat.LatestMessageId = &message.ID
-	r = DB.Save(&chat)
-
-	if r.Error != nil {
-		return r.Error
+	if err := DB.Save(&chat).Error; err != nil {
+		return err
 	}
 
 	return nil
