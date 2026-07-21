ee810be59837636d337e5522f90fe3c3e9c21ad5
saces <saces@c-base.org>
2026-07-21 16:05:55 +0200

    improve http transport logger

--- backend/utils/httplog/httplog.go
+++ backend/utils/httplog/httplog.go
@@ -1,45 +1,72 @@
 package httplog
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 )
 
+/*
+	simple http transport logger
+
+this is a low level transport logger, therefore the output contains all your precious secrets.
+this is a logger for development and debugging purposes uns should never be used in full mode in a
+production envirionment
+*/
 type loggingTransport struct {
-	full    bool
-	logBody bool
+	fullRequest     bool
+	fullResponse    bool
+	logRequestBody  bool
+	logResponseBody bool
 }
 
 func (s *loggingTransport) RoundTrip(r *http.Request) (*http.Response, error) {
-	var buf []byte
-	if s.full {
-		buf, _ = httputil.DumpRequestOut(r, s.logBody)
+	var buf []byte = []byte{}
+	var err error
+
+	if s.fullRequest {
+		buf, err = httputil.DumpRequestOut(r, s.logRequestBody)
+		if err != nil {
+			buf = fmt.Appendf(buf, "error dumping request %s %s %s\n", r.Host, r.Method, r.URL)
+		}
 	} else {
 		log.Printf("--> %s %s %s\n", r.Host, r.Method, r.URL)
 	}
 
 	resp, err := http.DefaultTransport.RoundTrip(r)
+	// err is returned after dumping the response
 
-	if s.full {
-		respBytes, _ := httputil.DumpResponse(resp, true)
-		buf = append(buf, respBytes...)
-		log.Printf("<-> %s\n%s\n", r.URL.String(), buf)
-	} else if resp != nil {
-		log.Printf("<-- %d %s\n", resp.StatusCode, resp.Request.URL)
+	if s.fullResponse {
+		if resp != nil {
+			respBytes, derr := httputil.DumpResponse(resp, s.logResponseBody)
+			if err != nil {
+				buf = fmt.Appendf(buf, "error dumping response %s %s %s:\n%#v", r.Host, r.Method, r.URL, derr)
+			} else {
+				buf = append(buf, respBytes...)
+			}
+			log.Printf("<-> %s\n%s\n", r.URL.String(), buf)
+		} else {
+			buf = fmt.Appendf(buf, "error obtaining response %s %s %s:\n%#v", r.Host, r.Method, r.URL, err)
+		}
+	} else {
+		if resp != nil {
+			log.Printf("<-- %d %s\n", resp.StatusCode, resp.Request.URL)
+		} else {
+			log.Printf("<-- error obtaining response %s %s %s:\n%#v", r.Host, r.Method, r.URL, err)
+		}
 	}
-
 	return resp, err
 }
 
-func filterAuthorization(headers http.Header) http.Header {
-	filteredHeaders := make(http.Header)
-	for k, v := range headers {
-		if k == "Authorization" {
-			filteredHeaders[k] = []string{"<removed>"}
-		} else {
-			filteredHeaders[k] = v
-		}
-	}
-	return filteredHeaders
+func NewSimpleLoggingTransport() *loggingTransport {
+	return NewLoggingTransport(false, false, false, false)
+}
+
+func NewFullLoggingTransport(logBody bool) *loggingTransport {
+	return NewLoggingTransport(true, true, logBody, logBody)
+}
+
+func NewLoggingTransport(logReq, logResp, logReqBody, logRespBody bool) *loggingTransport {
+	return &loggingTransport{logReq, logResp, logReqBody, logRespBody}
 }
